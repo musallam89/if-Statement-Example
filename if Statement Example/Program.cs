a61bf1ac0fd32dae35d0c68e79ce984288027b7b
musallam <musallam@DESKTOP-4JONBL2>
2023-02-26 18:50:58 +0300

    Add project files.

---
+++ if Statement Example/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("أهلا بك في برنامج اصدار رخصة القيادة. اضغط على أي مفتاح على لوحة المفاتيح للبدأ");
+Console.ReadKey();
+Console.WriteLine("كم عمرك؟");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x >= 18)
+{
+    Console.WriteLine("تهانينا أنت مؤهل لِ اصدار رخصة القيادة");
+}
+else if (x == 17)
+{
+    Console.WriteLine("أنت حاليا مؤهل لِ اصدار رخصة قيادة مؤقتة عمرها سنة واحدة");
+}
+else if (x < 18)
+{
+    Console.WriteLine("نأسف أنت غير مؤهل لِ اصدار رخصة القيادة لأنك لم تتجاوز السن القانوني.");
+}
